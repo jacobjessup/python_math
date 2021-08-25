b45eee90923934775843f622d29aed085adeb092
jacobjessup <jacobwjessup@gmail.com>
2021-08-25 11:00:04 -0400

    Update refraction.py
    - Added rounding to sin1/2 and totals1/2

--- refraction.py
+++ refraction.py
@@ -8,11 +8,15 @@ sin1:float
 
 sin1=(math.sin(math.radians(float(input("Input sin1 angle here: \n")))))
 
+sin1=round(sin1, 4)
+
 print(sin1)
 
 n1=(float(input("Input n1 here: \n")))
 
 total1=n1*sin1
+
+total1=round(total1, 4)
 
 print("The first angle of refraction is: \n", total1)
 
@@ -22,11 +26,15 @@ sin2:float
 
 sin2=(math.sin(math.radians(float(input("Input sin2 angle here: \n")))))
 
+sin2=round(sin2, 4)
+
 print(sin2)
 
 n2=(float(input("Input n2 here: \n")))
 
 total2=n2*sin2
+
+total2=round(total2, 4)
 
 print("The second angle of refraction is: \n", total2)
 
@@ -40,5 +48,6 @@ if total1 == total2:
 else:
     print("Something is wrong with the inputs of sin or n. \n")
     print("Please redo the inputs for this program.")
+    
     
     
