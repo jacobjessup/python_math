2cb21c05e60c1b0d207c30d186d2c88d13eeb152
jacobjessup <jacobwjessup@gmail.com>
2021-08-26 08:40:33 -0400

    Update refraction.py

--- refraction.py
+++ refraction.py
@@ -45,6 +45,7 @@ print("\n")
 
 if total1 == total2:
     print("Snell\'s law is validated!")
+    print("\n")
 else:
     print("Something is wrong with the inputs of sin or n. \n")
     print("Please redo the inputs for this program.")
